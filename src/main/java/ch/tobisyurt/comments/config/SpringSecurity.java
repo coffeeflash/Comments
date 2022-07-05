b7d118092f941f4f69500fe445179a0c895b3b69
Tobias Schneeberger <tobiassamuel.schneeberger@students.bfh.ch>
2022-07-05 13:00:54 +0200

    micro changes

--- src/main/java/ch/tobisyurt/comments/config/SpringSecurity.java
+++ src/main/java/ch/tobisyurt/comments/config/SpringSecurity.java
@@ -17,7 +17,11 @@ public class SpringSecurity {
         String[] resources = new String[]{
                 "/api/**",
                 "/","/static/**", "/apple-touch-icon.png", "/index.html", "/static/css/**",
-                "/static/js/**", "/favicon-32x32.png", "/favicon-16x16.png", "/site.webmanifest"
+                "/static/js/**", "/favicon-32x32.png", "/favicon-16x16.png", "/site.webmanifest",
+                "/browserconfig.xml", "/favicon.ico",
+                "/mstile-70x70.png", "mstile-144x144.png", "/mstile-150x150.png", "/mstile-310x150.png",
+                "/mstile-310x310.png", "/safari-pinned-tab.svg", "/android-chrome-512x512.png",
+                "/android-chrome-192x192.png"
         };
 
 
