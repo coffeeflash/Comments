1e3deadffc7f114f1bace0a9e19a6798e0f7bdcc
Tobias Schneeberger <tobiassamuel.schneeberger@students.bfh.ch>
2023-09-30 09:36:54 +0200

    fixed load

--- src/main/java/ch/tobisyurt/comments/controller/APIController.java
+++ src/main/java/ch/tobisyurt/comments/controller/APIController.java
@@ -1,23 +1,16 @@
 package ch.tobisyurt.comments.controller;
 
-import ch.tobisyurt.comments.model.Comment;
-import ch.tobisyurt.comments.model.CommentReq;
 import ch.tobisyurt.comments.model.Quiz;
-import ch.tobisyurt.comments.service.CommentsService;
 import ch.tobisyurt.comments.service.MemCacheService;
 import ch.tobisyurt.comments.service.QuizService;
-import ch.tobisyurt.comments.service.SecUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,9 +21,6 @@ public class APIController {
 
     @Autowired
     private QuizService quizService;
-
-    @Autowired
-    private CommentsService commentsService;
 
     @Autowired
     private MemCacheService memCacheService;
@@ -57,22 +47,20 @@ public class APIController {
     }
 
     @GetMapping(value = "/load")
-    public List<Comment> getLoad(@RequestParam Integer count) {
+    public List<Integer> getLoad(@RequestParam Integer count) {
         LOG.info("{} got called for post: {}", "/load",count);
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             list.add(i);
         }
+        if(list.size() > 10000) {
+            return new ArrayList<Integer>(100);
 
-        return commentsService.getComments("asd");
+        }
+
+        return list;
     }
 
-
-    @GetMapping(value = API_MAPPING_GET_COMMENTS)
-    public List<Comment> getComments(@RequestParam String source) {
-        LOG.info("{} got called for post: {}", API_MAPPING_GET_COMMENTS, source);
-        return commentsService.getComments(source);
-    }
 
     @PostMapping(value = API_MAPPING_POST_QUIZ_SOLUTION)
     public void verifyQuizSolution(@RequestParam List<String> nonceStrings, @RequestParam String quizId){
@@ -80,34 +68,6 @@ public class APIController {
         boolean nonceValid = quizService.verifyQuizSolution(quizId, nonceStrings);
         LOG.info("nonceValid: {}", nonceValid);
 
-    }
-
-    @PostMapping(value = API_MAPPING_POST_COMMENT)
-    public String addComment(HttpServletRequest request, @RequestBody CommentReq commentReq){
-
-        LOG.info("{} got called for post: {}", API_MAPPING_POST_COMMENT, commentReq.getSourceTitle());
-        LOG.info("Ip block time: {}", ipBlockTime);
-        // somehow request.getRemoteUser() does not convert to a proper String... no hashCode...
-        String clientIpAddress = "" + request.getRemoteUser();
-        LOG.info("Client IP Address is: {}", request.getRemoteAddr());
-
-        if(memCacheService.get(clientIpAddress) != null) {
-            return "You can only post a comment every " + ipBlockTime / 60 + " minutes.";
-        }
-
-        if(!SecUtil.validate(commentReq.getComment())
-                || !SecUtil.validate(commentReq.getUser())
-                || !quizService.verifyQuizSolution(commentReq.getQuizId(), commentReq.getQuizSolutions())){
-
-            LOG.info("rejected this request! (Either a nonce is invalid or you tried to bypass the input validation)");
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
-
-        memCacheService.add(clientIpAddress, "waiting", ipBlockTime);
-        commentReq.setComment(SecUtil.newLines(commentReq.getComment()));
-        commentsService.addComment(commentReq);
-
-        return "ok";
     }
 
     // to ensure nothing gets out for security reasons
